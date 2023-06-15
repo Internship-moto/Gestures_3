e02f66f128436cafba4e8637c681fbe4c4ae9bea
Alex1iv <ivanov.politehnik@gmail.com>
2023-06-15 20:13:10 +0600

    clean functions

--- utils/figures.py
+++ utils/figures.py
@@ -19,92 +19,7 @@ from utils.functions import config_reader#, get_nogo, postporocessing_nogo
 # import constants from the config
 config = config_reader('../config/data_config.json') 
 
-def get_nogo(df:np.array)->np.array:
-    """Gets 'nogo' condition from all sensors
 
-    Args:
-        df (np.array): _description_
-
-    Returns:
-        np.array: _description_
-    """   
-    # Get diff for all sensors . Shape = (28975, 5) 
-    nogo = np.diff(df, axis=0) 
-
-    # Get changes vector. Shape = (28975, 1)
-    Nogo = np.sum(np.abs(nogo), axis=1)#, keepdims=True
-    
-    return np.where(Nogo>1,1,0)
-
-def postporocessing_nogo(arr:np.array)->np.array:
-    """Delete sponatious single peaks from the given array
-    Args:
-        arr (np.array): data array
-    Returns:
-        np.array: _description_
-    """
-    # initial value
-    i = 0
-
-    for i in range(len(arr)):
-        
-        step = arr[i:i+4]
-        try:
-            # case [0,0,0,0] or [1,1,1,1]. Смещаемся на 1 элемент вправо
-            if step[3]==step[2]: 
-                i+=1 
-                #step = arr[i:i+4]
-                
-            # case [0,0,0,1]. Смещаемся на 2 элемента вправо
-            else:
-                i+=2
-                step = arr[i:i+4]
-                
-                #case [1,0,0,0]
-                if step[1]==step[2]==step[3]:
-                    step[0]==step[1]
-                    i+=1
-                
-                #case [1,0,1,0] and [1,0,1,1]
-                else:  
-                    step[1]=step[0]
-                    i+=1
-        except:
-            break
-    
-    return arr
-
-
-def get_all_sensors_plot(id:int, X_train:pd.DataFrame, plot_counter:int=None):
-    """
-    Plots sensor signals by arguments:
-     
-    id(int) - test number;
-    X_train(pd.dataFrame) - training sample;
-    plot_counter(int) - figure number.
-    """
-    
-    fig = px.line(data_frame=X_train[id].T)
-    
-    fig.update_layout( 
-        xaxis_title_text = 'Periods', 
-        yaxis_title_text = 'Sensor signals', # yaxis_range = [0, 3000],
-        legend_title_text= 'Sensor <br> index',
-        width=600, height=400,  margin=dict(l=20, r=20, t=20, b=100),
-    )
-
-    fig.show()
-
-    # # сохраним результат в папке figures. Если такой папки нет, то создадим её
-    # if not os.path.exists("figures"):
-    #     os.mkdir("figures")
-
-    if plot_counter is not None:
-        fig.write_image(f'../figures/fig_{plot_counter}.png', engine="kaleido")
-    else: 
-        plot_counter = 1
-        
-    fig.update_layout(title=dict(text=f'Рис. {plot_counter} - Sensor signals <br> for the test ' + str(id), x=.5, y=0.08, xanchor='center'))
 
 
 def get_signals_plot(
@@ -125,48 +40,23 @@ def get_signals_plot(
     fig, ax = plt.subplots(2, 1, sharex=True, figsize=(12, 6))
     ax[0].plot(X_train)
     ax[0].set_title('OMG')
+    
     ax[1].plot(y_train + dist)  
     ax[1].yaxis.set_ticks(dist, labels=GLOVE_CH)
     ax[1].set_title('Glove')
-
+    
     
     if title is not None:
-        fig.suptitle(title, fontsize=14)
-        
-    fig.tight_layout()
+        fig.suptitle(title, y=0.01, fontsize=14)
+   
     
     if plot_counter is not None:
         plt.savefig(f'../figures/fig_{plot_counter}.png')
+    
+    plt.tight_layout()
+    #plt.show();
 
-
-
-def get_nogo_plot(y_train:np.array, limits:tuple, GLOVE_CH:list=config.GLOVE_CH):
-    """Displays nogo status chart
-
-    Args:
-        y_train (np.array): targets. Default: 5 dependent variables
-        low_lim (int): low limit
-        high_lim (int): high limit
-        GLOVE_CH (list, optional): Target names list. Defaults to config.GLOVE_CH.
-    """    
-    GLOVE_CH = GLOVE_CH[:-1]
-    low_lim, high_lim = limits[0], limits[1]
-    dist = -np.arange(5)*200
-
-    fig, ax = plt.subplots(3, 1, sharex=True, figsize=(10, 6)) 
-    ax[0].plot(np.arange(low_lim, high_lim), y_train[low_lim:high_lim] + dist)  
-    ax[0].yaxis.set_ticks(dist, GLOVE_CH) 
-    ax[0].set_title('Glove')
-
-    ax[1].plot(np.arange(low_lim, high_lim), get_nogo(y_train)[low_lim:high_lim])
-    ax[1].set_title('Nogo')
-    ax[1].yaxis.set_ticks(np.linspace(0,1,2), ['Nogo', 'Go'])
-
-    ax[2].plot(np.arange(low_lim, high_lim), postporocessing_nogo(get_nogo(y_train))[low_lim:high_lim])
-    ax[2].set_title('Nogo postprocessed')
-    ax[2].yaxis.set_ticks(np.linspace(0,1,2), ['Nogo', 'Go']);
-
-   
+  
     
 
 def get_signals_comparison_plot( 
@@ -236,15 +126,18 @@ def get_signals_comparison_plot(
         plt.tight_layout()
         
     if plot_counter is not None:
-        plt.suptitle(f"Fig.{plot_counter} - Model learning",  fontsize=14) # y=-0.1,
-        plt.write_image(f'../figures/fig_{plot_counter}.png', engine="kaleido")
+        plt.suptitle(f"Fig.{plot_counter} - Истинные координаты и предсказания", y=0.05, fontsize=14)
+        plt.savefig(config.PATH_FIGURES + f'fig_{plot_counter}.png')
+    else: 
+        plot_counter = 1
+        plt.suptitle(f"Fig.{plot_counter} - Истинные координаты и предсказания", y=0.05, fontsize=14)
         
         
-def plot_history(history, title:str=None, plot_counter:int=None):
+def plot_history(history, model_name:str=None, plot_counter:int=None):
     """Функция визуализации процесса обучения модели.
     Аргументы:
     history (keras.callbacks.History) - история обучения модели,
-    title (str) - figure title. Use: model.name
+    model_name (str) - figure title. Use: model.name
     plot_counter (int) - порядковый номер рисунка.      
     """
     mse_metric = history.history['mse'] 
@@ -272,38 +165,14 @@ def plot_history(history, title:str=None, plot_counter:int=None):
     ax[1].set_title(f"Training and validation MSE")
     ax[1].legend()
 
-    
     if plot_counter is not None:
-        plt.suptitle(f"Fig.{plot_counter} - Model learning",  fontsize=14) # y=-0.1,
-        plt.write_image(f'../figures/fig_{plot_counter}.png', engine="kaleido") #savefig(...)
+        plt.suptitle(f"Fig.{plot_counter} - {model_name} learning", y=0.05, fontsize=14)
+        plt.savefig(config.PATH_FIGURES + f'fig_{plot_counter}.png')
     
     else: 
         plot_counter = 1
-        plt.suptitle(f"Fig.{plot_counter} - {title} learning", y=1, fontsize=14) # y=-0.1,
+        plt.suptitle(f"Fig.{plot_counter} - {model_name} learning", y=-0.1, fontsize=14)  
+    plt.tight_layout();
 
- 
     # fig.show(); #- не вызывать для корретного логгирования
     
-def web_plot(df:pd.DataFrame, GLOVE_CH:list=config.GLOVE_CH):
-    GLOVE_CH = GLOVE_CH[:-1]
-    # Number of variables we're plotting.
-    num_vars = len(GLOVE_CH)
-
-    # Split the circle into even parts and save the angles so we know where to put each axis.
-    angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
-    angles += angles[:1]
-
-    # ax = plt.subplot(polar=True)
-    fig, ax = plt.subplots(figsize=(7, 7), subplot_kw=dict(polar=True))
-        
-    for i in range(df.shape[1]):
-        
-        values = df.values[:,i].tolist() 
-        # The plot is a circle, so we need to "complete the loop" and append the start value to the end.
-        values += values[:1]
-        
-        ax.plot(angles, values, linewidth=1, label=df.columns[i]) #, color='red'
-        # Fill it in.
-        ax.fill(angles, values, alpha=0.75)# , color='red'
-        ax.set_thetagrids(np.array(angles[:-1]) * 180/np.pi, GLOVE_CH);
-        plt.legend();
